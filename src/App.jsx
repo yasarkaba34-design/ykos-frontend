e12c5cc7441c7363fee6c613c2b1799f587c800c
Yasar Kaba <yasarkaba34@gmail.com>
2026-07-19 14:14:56 +0300

    App.jsx import eksigi tamamen giderildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import YKOSDashboard from "./pages/YKOSDashboard";
+import YKOSDashboard from "./modules/YKOSDashboard";
+import SemanticEngine from "./components/SemanticEngine";
+import AtlasMap from "./components/AtlasMap";
+import LayersSystem from "./components/LayersSystem";
+
+
+
 
 export default function App() {
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({
-  json: {
-    stringify: true
-  }
-});
 
 export default defineConfig({
   plugins: [react()],
+  json: {
+    stringify: true
+  }
 })
