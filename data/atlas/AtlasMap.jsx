5e1451b79b81119db47b16a6bb74367a38f1f025
Yasar Kaba <yasarkaba34@gmail.com>
2026-07-19 19:36:52 +0300

    AudioEngine ses dosyası yolları hizalandı

--- data/atlas/AtlasMap.jsx
+++ data/atlas/AtlasMap.jsx
@@ -1,7 +1,10 @@
-import AtlasMap from "../data/atlas/AtlasMap";
 import { YKOS_GEO } from "../data/ycos_geo";
+
 const getGeoForRoot = (root) => {
   return YKOS_GEO[root.toUpperCase()] || null;
 };
-geo: YKOS_GEO[item.step] || null
 
+// Örnek kullanım:
+const geo = YKOS_GEO[item.step] || null;
+
+export default getGeoForRoot;
