132c3564e4db65458c490dd77c64f25b56aebe63
Yasar Kaba <yasarkaba34@gmail.com>
2026-07-19 20:29:34 +0300

    AtlasMap veri importundaki uc nokta hatasi ve mukerrer satirlar temizlendi

--- data/atlas/AtlasMap.jsx
+++ data/atlas/AtlasMap.jsx
@@ -1,10 +1,5 @@
-import YKOS_GEO from "./ykos_core.json";
-import MapComponent from "./MapComponent"; // varsa
-
-const getGeoForRoot = (root) => {
-  if (!root) return null;
-  return YKOS_GEO[root.toUpperCase()] || null;
-};
+import YKOS_GEO from "../ykos_core.json";
+import MapComponent from "./MapComponent";
 
 const AtlasMap = ({ item }) => {
   const geo = getGeoForRoot(item?.step);
