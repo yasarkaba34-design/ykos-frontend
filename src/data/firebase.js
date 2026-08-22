1fd0d4c7c041310fccb3962d75f503c5f149d415
Yasar Kaba <yasarkaba34@gmail.com>
2026-08-22 10:32:16 +0300

    Güvenlik: Firebase API anahtarları .env dosyasına taşındı

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9aR79SUUUJBQfAWVpIj6z7LGSzHbKhnI",
-  authDomain: "ykos-database.firebaseapp.com",
-  projectId: "ykos-database",
-  storageBucket: "ykos-database.firebasestorage.app",
-  messagingSenderId: "1046957978671",
-  appId: "1:1046957978671:web:56f66d450f5b32e265c2da",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Firebase yalnızca bir kez başlatılır.
